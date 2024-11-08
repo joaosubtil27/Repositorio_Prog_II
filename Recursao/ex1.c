26f0d310897890c0b6526ab65f8d12870c9c87cd
jaasubtil <joaoarthursubtil@gmail.com>
2024-11-07 22:12:28 -0300

    Hello World!

--- Recursao/ex1.c
+++ Recursao/ex1.c
@@ -11,14 +11,13 @@ int main()
 {
 
     char palavras[1000];
-
     int i = 0;
 
     while (scanf("%c", &palavras[i]) != EOF)
     {
         i++;
     }
-    palavras[i+1] = '\0';
+    palavras[i] = '\0';
     ImprimeInvertido(palavras);
 }
 
@@ -26,24 +25,22 @@ void ImprimeInvertido(char *string)
 {
     int i;
 
-    while (string[inicio_p2] != ' ')
-    {   
-        inicio_p2++;
-    }
-
-    for (i = inicio_p2; i >= fim_p1; i--)
-    {   if (inicio_p2 != '\0')
-        printf("%c", string[i]);
-
-        else
-        printf("%c", string[i-1]);
-    }
-    
-    if (string[inicio_p2+1] != '\0')
+    while (string[inicio_p2] != ' ' && string[inicio_p2] != '\0')
     {
-        fim_p1 = inicio_p2;
         inicio_p2++;
-        
+    }
+
+    for (i = inicio_p2 - 1; i >= fim_p1; i--)
+    {
+        printf("%c", string[i]);
+    }
+
+    if (string[inicio_p2] != '\0')
+    {
+        printf(" ");
+        fim_p1 = inicio_p2 + 1;
+        inicio_p2++;
+
         ImprimeInvertido(string);
     }
 }
