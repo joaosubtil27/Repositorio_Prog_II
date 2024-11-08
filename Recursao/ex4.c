a378d2c176c99d20bf8bfc3e62b15c7f9e34c4dc
jaasubtil <joaoarthursubtil@gmail.com>
2024-11-08 17:44:56 -0300

    Recursao/

--- Recursao/ex4.c
+++ Recursao/ex4.c
@@ -7,41 +7,47 @@
 int palindromo(char *string, int tamanho);
 
 int elem_lidos = 0;
-int validacao=1;
+int validacao = VERDADEIRO;
 int main()
 {
 
     int qnt_letras = 0;
     char palavra[100];
-    while (scanf("%c", &palavra[qnt_letras]) != '\0')
-    {
-        if (palavra[qnt_letras] == '\n')
-        {
 
-            if (palindromo(palavra, qnt_letras - 1) == VERDADEIRO)
+    while (palavra[qnt_letras] != EOF)
+    {
+        scanf("%c", &palavra[qnt_letras]);
+
+
+        if (palavra[qnt_letras] == '\n' || palavra[qnt_letras] == EOF)
+        {
+            if (palindromo(palavra, qnt_letras-1) == VERDADEIRO)
                 printf("SIM\n");
 
             else
+            {
                 printf("NAO\n");
+            }
 
             qnt_letras = 0;
             validacao = VERDADEIRO;
             elem_lidos = 0;
         }
-        else
-            qnt_letras++;
+
     }
+
 }
 
-int palindromo(char *string, int tamanho){
+int palindromo(char *string, int tamanho)
+{
 
-if (string[elem_lidos] == string[tamanho-elem_lidos]){
-elem_lidos++;
-validacao = palindromo(string, tamanho);
-}
-else 
-validacao = FALSO;
+    if (string[elem_lidos] == string[tamanho - elem_lidos - 1])
+    {
+        elem_lidos++;
+        validacao = palindromo(string, tamanho);
+    }
+    else
+        validacao = FALSO;
 
-return validacao;
-
+    return validacao;
 }
