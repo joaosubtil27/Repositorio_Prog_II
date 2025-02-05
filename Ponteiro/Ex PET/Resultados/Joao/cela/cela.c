84cf9e6b4c08a903c04637579a5b4a01d74b62e7
jaasubtil <joaoarthursubtil@gmail.com>
2025-02-04 22:25:43 -0300

    Ponteiros

---
+++ Ponteiro/Ex PET/Resultados/Joao/cela/cela.c
@@ -0,0 +1,100 @@
+#include "prisioneiro.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+#define MAXTAM_CELAS 100
+
+typedef struct
+{
+    int capacidade;
+    int nPresidiarios;
+    tPrisioneiro prisioneiros[MAXTAM_PRISIONEIRO];
+} tCela;
+
+/**
+ * @brief Cria uma cela da prisão a partir do parâmetro da capacidade das celas.
+ * @param capacidade numero máximo de presidiários na cela
+ */
+tCela criaCela(int capacidade)
+{
+    tCela cela;
+    cela.capacidade = capacidade;
+    cela.nPresidiarios = 0;
+    return cela;
+}
+
+/**
+ * @brief Retorna valor diferente de zero caso haja uma vaga na cela, e 0 caso contrario.
+ * @param cela Cela que se deseja saber se há vaga
+ */
+int possuiVagaCela(tCela *cela)
+{
+    if (cela->capacidade > cela->nPresidiarios)
+        return 1;
+    else
+        return 0;
+}
+
+/**
+ * @brief Retorna o numero atual de prisioneiros na cela
+ */
+int obtemNumeroPrisioneirosCela(tCela *cela)
+{
+    return cela->nPresidiarios;
+}
+
+/**
+ * @brief Insere o prisioneiro na cela, após já ter garantido que há vaga na cela
+ */
+void inserePrisioneiroCela(tCela *cela, tPrisioneiro prisioneiro)
+{
+        cela->prisioneiros[cela->nPresidiarios] = prisioneiro;
+        cela->nPresidiarios++;
+}
+
+/**
+ * @brief Retorna o presidiário da cela na posição fornecida
+ * @param cela Cela que se deseja obter o prisioneiro
+ * @param i Posição do prisioneiro desejado no vetor de prisioneiros da cela
+ */
+tPrisioneiro obtemPrisioneiroCela(tCela *cela, int i)
+{
+    return cela->prisioneiros[i];
+}
+
+/**
+ * @brief Chama a função individual de fuga para cada prisioneiro da cela
+ * @param cela Cela onde ocorre a fuga
+ */
+void fogePrisioneirosCela(tCela *cela)
+{
+    for (int i = 0; i <= cela->nPresidiarios; i++)
+    {
+        fogePrisioneiro(&cela->prisioneiros[i]);
+        cela->nPresidiarios--;
+    }
+}
+
+/**
+ * @brief Chama a função individual de passar o dia para cada prisioneiro da cela
+ * @param cela cela fornecida para o passar do dia
+ */
+void passaDiaCela(tCela *cela)
+{
+    for (int i = 0; i <= cela->nPresidiarios; i++)
+    {
+
+        passaTempoPrisioneiro(&cela->prisioneiros[i]);
+        if (acabouPenaPrisioneiro(&cela->prisioneiros[i]) == 1)
+        {
+            liberaPrisioneiroCumpriuPena(&cela->prisioneiros[i]);
+            for (int j = i + 1; j <= cela->nPresidiarios; j++)
+            {
+                cela->prisioneiros[j - 1] = cela->prisioneiros[j];
+            }
+            passaTempoPrisioneiro(&cela->prisioneiros[i]);
+            cela->nPresidiarios--;
+        }
+    }
+}
---
+++ Ponteiro/Ex PET/Resultados/Joao/completo/prisao.c
@@ -0,0 +1,176 @@
+#include "prisao.h"
+
+#define MAXTAM_NOME 100
+
+/**
+ * @brief Construtor do tipo prisão
+ * @param nome Nome da prisão
+ * @param nCelas Número de celas ocupadas
+ * @param capacidadeCelas Número total de celas
+ */
+
+tPrisao criaPrisao(char *nome, int nCelas, int capacidadeCelas)
+{
+    tPrisao prisao;
+    strcpy(prisao.nome, nome);
+    prisao.nCelas = nCelas;
+    prisao.capacidadeCelas = capacidadeCelas;
+    prisao.dia = 0;
+    for (int i = 0; i < prisao.nCelas; i++)
+    {
+        prisao.celas[i] = criaCela(capacidadeCelas);
+    }
+    return prisao;
+}
+
+/**
+ * @brief Contém o loop do programa que deve imprimir o menu e selecionar opção
+ */
+
+void executaPrisao(tPrisao *prisao)
+{
+    char comando;
+    while (1)
+    {
+        imprimeDiaEMenuPrisao(prisao);
+
+        scanf("%c\n", &comando);
+        if (comando == 'I')
+        {
+            printf("Informe a pena e o nome do prisioneiro:\n");
+            inserePrisioneiroPrisao(prisao);
+        }
+        else if (comando == 'P')
+        {
+            passaDiaPrisao(prisao);
+        }
+        else if (comando == 'F')
+        {
+            printf("Informe a cela em qual ocorreu a fuga:\n");
+            registraFugaCelaPrisao(prisao);
+        }
+        else if (comando == 'E')
+        {
+            finalizaPrograma(prisao);
+            break;
+        }
+        if (obtemNumeroPrisioneirosPrisao(prisao) == prisao->capacidadeCelas * prisao->nCelas)
+        {
+            rebeliaoGeralPrisao(prisao);
+        }
+    }
+}
+
+/**
+ * @brief Imprime o menu
+ */
+
+void imprimeDiaEMenuPrisao(tPrisao *prisao)
+{
+    printf("Dia %d na prisao de %s\n", prisao->dia, prisao->nome);
+    printf("Menu:\n");
+    printf("I - Inserir detento\n");
+    printf("P - Passar o dia\n");
+    printf("F - Registrar fuga de cela\n");
+    printf("E - Encerrar programa\n");
+}
+
+/**
+ * @brief Faz a leitura e adiciona o prisioneiro na prisão
+ */
+
+void inserePrisioneiroPrisao(tPrisao *prisao)
+{
+    int tempo_sentenca;
+    char nome[MAXTAM_NOME];
+    int i = 0;
+    tPrisioneiro prisioneiro;
+    scanf("%d %[^\n]\n", &tempo_sentenca, nome);
+    if (tempo_sentenca <= 0)
+        printf("Valor invalido\n");
+    else
+    {
+        prisioneiro = criaPrisioneiro(nome, tempo_sentenca);
+        while (1)
+        {
+            if (i == prisao->nCelas)
+                break;
+
+            else if (possuiVagaCela(&prisao->celas[i]) != 0)
+            {
+                inserePrisioneiroCela(&prisao->celas[i], prisioneiro);
+                printf("Detento %s foi inserido na cela %d\n", prisioneiro.nome, i);
+                break;
+            }
+            i++;
+        }
+    }
+}
+/**
+ * @brief Passa o dia em cada uma das celas da prisão
+ */
+void passaDiaPrisao(tPrisao *prisao)
+{
+    for (int i = 0; i < prisao->nCelas; i++)
+    {
+        passaDiaCela(&prisao->celas[i]);
+    }
+    prisao->dia++;
+}
+/**
+ * @brief Le da entrada padrão o índice da cela no qual ocorreu a fuga
+ */
+void registraFugaCelaPrisao(tPrisao *prisao)
+{
+    int num_cela;
+    scanf("%d\n", &num_cela);
+    if (num_cela < 0)
+        printf("Valor invalido\n");
+    else
+        fogePrisioneirosCela(&prisao->celas[num_cela]);
+}
+
+/**
+ * @brief Finaliza o programa, havendo diferença na finalização caso tenha ou nao prisioneiros na prisao
+ */
+
+void finalizaPrograma(tPrisao *prisao)
+{
+    if (obtemNumeroPrisioneirosPrisao(prisao) > 0)
+    {
+        printf("Prisioneiros liberados para a finalizacao do programa!!!\n");
+    }
+    else if (obtemNumeroPrisioneirosPrisao(prisao) == 0)
+    {
+        printf("Fim do programa.\n");
+    }
+}
+
+/**
+ * @brief Obtém o total de prisioneiros
+ */
+
+int obtemNumeroPrisioneirosPrisao(tPrisao *prisao)
+{
+    int soma = 0;
+    for (int i = 0; i < prisao->nCelas; i++)
+    {
+        soma += prisao->celas[i].nPresidiarios;
+    }
+    return soma;
+}
+
+/**
+ * @brief Imprime mensagem de rebelião e realiza fuga de todos os prisioneiros
+ */
+
+void rebeliaoGeralPrisao(tPrisao *prisao)
+{
+    printf("REBELIAO GERAL\n");
+    for (int i = 0; i < prisao->nCelas; i++)
+    {
+        fogePrisioneirosCela(&prisao->celas[i]);
+    }
+    finalizaPrograma(prisao);
+    exit(1);
+}
